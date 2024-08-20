0c19144a155a50ac2173b01d5524e21735ba076d
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-20 08:42:44 +0200

    wip
    create component and call it

---
+++ src/components/Chat.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import ChatBot from "react-chatbotify"
+function App() {
+    const [count, setCount] = useState(0)
+
+    const flow = {
+        "start": {
+            "message": "Hello world!"
+        }
+    }
+    return (
+        <>
+            <ChatBot flow={flow} />
+        </>
+    )
+}
+
+export default App
